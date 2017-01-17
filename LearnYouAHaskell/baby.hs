771c27510796c3ecf7be6984e19232f263dbb2df
Christian Mello <ecksian@icloud.com>
2017-01-16 19:57:38 -0800

    Initial commit. Added LYAH folder.

---
+++ LearnYouAHaskell/baby.hs
@@ -0,0 +1,7 @@
+doubleMe x = x + x
+doubleUs x y = doubleMe x + doubleMe y
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x*2
+
+nameFunc x = "It's a-me, " ++ x ++ "!"
