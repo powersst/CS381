95f16945f8dd5d2ff320b2ff5d1c3712ed6374b0
Christian Mello <ecksian@icloud.com>
2017-02-23 12:10:59 -0800

    Added placeholder code to shut up compiler

--- Homework/Homework4/KarelSemantics.hs
+++ Homework/Homework4/KarelSemantics.hs
@@ -11,13 +11,14 @@ import KarelState
 test :: Test -> World -> Robot -> Bool
 test (Not t)    w r = not (test t w r)
 test (Facing c) _ r = getFacing r == c
--- test (Clear d)  w r =
+test (Clear _)  _ _ = undefined
 test Beeper     w r = hasBeeper (getPos r) w
 test Empty      _ r = isEmpty r
 
 -- | Valuation function for Stmt.
 stmt :: Stmt -> Defs -> World -> Robot -> Result
 stmt Shutdown     _ _ r = Done r
+stmt Move         _ _ _ = undefined
 -- stmt Move       _ w r = let p = getPos r
 --                         in if isClear p w
 --                               then OK _ (setPos)
@@ -30,12 +31,17 @@ stmt PutBeeper      _ w r = let p = getPos r
                                 then OK (incBeeper p w) (decBag r)
                                 else Error "No beeper in bag!"
 stmt (Turn d)       _ w r = OK w (setFacing (cardTurn d (getFacing r)) r)
+stmt (Call _)       _ _ _ = undefined
+stmt (Iterate _ _)  _ _ _ = undefined
 stmt (If t st se)   d w r = let b = test t w r
                             in if b then stmt st d w r
                                     else stmt se d w r
 stmt (While t sb)   d w r = let b = test t w r
                             in if b then stmt (While t sb) d w r
                                     else OK w r
+stmt (Block [_])    _ _ _ = undefined
+stmt (Block [])    _ _ _ = undefined
+stmt (Block (_:_))    _ _ _ = undefined
 -- stmt (Block [ss]) d w r  = stmt ss d w r
 -- stmt [] d w r = OK w r
 -- stmt (s:ss) d w r = stmt ss (stmt s d w r)
