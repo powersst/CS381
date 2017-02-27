c73b89e79fd2ae754085e6606861ee2d88805463
Christian Mello <ecksian@icloud.com>
2017-02-27 14:43:58 -0800

    Finished HW4

--- Homework/Homework4/KarelSemantics.hs
+++ Homework/Homework4/KarelSemantics.hs
@@ -17,31 +17,43 @@ test Empty      _ r = isEmpty r
 
 -- | Valuation function for Stmt.
 stmt :: Stmt -> Defs -> World -> Robot -> Result
-stmt Shutdown      _ _ r = Done r
-stmt Move          _ _ _ = undefined
--- stmt Move       _ w r = let p = getPos r
---                         in if isClear p w
---                               then OK _ (setPos)
-stmt PickBeeper    _ w r = let p = getPos r
+stmt Shutdown       _ _ r = Done r
+stmt Move           _ w r = let p = getPos r
+                            in if isClear (neighbor (getFacing r) p) w
+                                then OK w (setPos (neighbor (getFacing r) p) r)
+                                else Error ("Blocked at: " ++ show (neighbor (getFacing r) p))
+stmt PickBeeper     _ w r = let p = getPos r
                             in if hasBeeper p w
                                 then OK (decBeeper p w) (incBag r)
                                 else Error ("No beeper to pick at: " ++ show p)
-stmt PutBeeper     _ w r = let p = getPos r
+stmt PutBeeper      _ w r = let p = getPos r
                             in if not (isEmpty r)
                                 then OK (incBeeper p w) (decBag r)
-                                else Error "No beeper in bag!"
-stmt (Turn d)      _ w r = OK w (setFacing (cardTurn d (getFacing r)) r)
-stmt (Call _)      _ _ _ = undefined
-stmt (Iterate _ _) _ _ _ = undefined
-stmt (If t st se)  d w r = let b = test t w r
+                                else Error "No beeper to put."
+stmt (Turn d)       _ w r = OK w (setFacing (cardTurn d (getFacing r)) r)
+stmt (Call m)       d w r = case lookup m d of
+                                (Just b) -> stmt b d w r
+                                _        -> Error ("Undefined macro: " ++ m)
+stmt (Iterate i s)  d w r = if i > 0 then case stmt s d w r of
+                                        (OK w' r') -> stmt (Iterate (i-1) s) d w' r'
+                                        (Done r') -> Done r'
+                                        (Error m) -> Error m
+                                     else OK w r
+
+stmt (If t st se)   d w r = let b = test t w r
                             in if b then stmt st d w r
                                     else stmt se d w r
-stmt (While t sb)  d w r = let b = test t w r
-                            in if b then stmt (While t sb) d w r
-                                    else OK w r
--- stmt (Block [])    _ w r = OK w r
--- stmt (Block (h:t)) d w r = case
+stmt (While t sb)   d w r = if test t w r then case stmt sb d w r of
+                                        (OK w' r') -> stmt (While t sb) d w' r'
+                                        (Done r') -> Done r'
+                                        (Error m) -> Error m
+                                     else OK w r
+stmt (Block [])     _ w r = OK w r
+stmt (Block (s:ss)) d w r = case stmt s d w r of
+                              (OK w' r') -> stmt (Block ss) d w' r'
+                              (Done r') -> Done r'
+                              (Error m) -> Error m
 
 -- | Run a Karel program.
---prog :: Prog -> World -> Robot -> Result
---prog (m,s) w r = stmt s m w r
+prog :: Prog -> World -> Robot -> Result
+prog (m,s) w r = stmt s m w r
